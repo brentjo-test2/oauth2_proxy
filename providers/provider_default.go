25e80336f07b621df5def1bef8811c5d410334b5
brentjo-test2 <brentjo+test2@github.com>
2018-05-11 16:24:22 -0700

    Update provider_default.go

--- providers/provider_default.go
+++ providers/provider_default.go
@@ -19,7 +19,6 @@ func (p *ProviderData) Redeem(redirectURL, code string) (s *SessionState, err er
 	}
 
 	params := url.Values{}
-	params.Add("redirect_uri", "http://halp.localhost/oauth2/callback")
 	params.Add("client_id", p.ClientID)
 	params.Add("client_secret", p.ClientSecret)
 	params.Add("code", code)
